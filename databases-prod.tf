3c59e8550c9562b1ab807261a4ecb3317340eb0e
Alvaro Rodriguez Gonzalez <alvamige@gmail.com>
2025-07-04 12:51:02 +0200

    Fix terraform format

--- databases-prod.tf
+++ databases-prod.tf
@@ -5,8 +5,8 @@ module "spring_boot_template_prod" {
   database_name = "spring-boot-template-prod"
   username      = "spring-boot-template-prod-user"
   password      = "secure_password_123"
-  
-  depends_on    = [time_sleep.wait_for_db]
+
+  depends_on = [time_sleep.wait_for_db]
 
   providers = {
     postgresql = postgresql.prod
--- databases-test.tf
+++ databases-test.tf
@@ -5,8 +5,8 @@ module "spring_boot_template_test" {
   database_name = "spring-boot-template-test"
   username      = "spring-boot-template-test-user"
   password      = "secure_password_123"
-  
-  depends_on    = [time_sleep.wait_for_db]
+
+  depends_on = [time_sleep.wait_for_db]
 
   providers = {
     postgresql = postgresql.test
